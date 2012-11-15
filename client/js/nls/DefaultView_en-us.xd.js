45ecd39ce243f32afcc80eff3af7f8048f8780af
Mac Angell <maca@ecollege.com>
2012-11-15 14:56:30 -0700

    work damnit

---
+++ client/js/nls/DefaultView_en-us.xd.js
@@ -0,0 +1,7 @@
+dojo._xdResourceLoaded(function(dojo, dijit, dojox){
+return {depends: [["provide", "coursehome.nls.DefaultView_en-us"],
+["provide", "dijit.nls.loading"],
+["provide", "dijit.nls.loading.en_us"]],
+defineResource: function(dojo, dijit, dojox){dojo.provide("coursehome.nls.DefaultView_en-us");dojo.provide("dijit.nls.loading");dijit.nls.loading._built=true;dojo.provide("dijit.nls.loading.en_us");dijit.nls.loading.en_us={"loadingState":"Loading...","errorState":"Sorry, an error occurred"};
+
+}};});
