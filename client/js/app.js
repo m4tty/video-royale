c3f0b7cafaf9ac97a9dd0700b990d48ae711c5c3
SCHWTA2 <timothys@ecollege.com>
2012-11-15 14:08:19 -0700

    Adding popcorn

--- client/js/app.js
+++ client/js/app.js
@@ -2,6 +2,30 @@ var App = function() {
 
 	this.initialize = function(videoId) {
 		$.get("/videos/" + videoId + "/royale", function(data){
+
+			$("#videoSrc").attr("src", data.video.url);
+
+			// Create a popcorn instance by calling Popcorn("#id-of-my-video")
+			var popcorn = Popcorn("#video");
+
+			for(var i=0; i<data.contentFrames.length; i++) {
+				var contentFrame = data.contentFrames[i];
+				
+				(function(index) {
+					popcorn.code({
+						start: Math.floor((contentFrame.startTime / 1000),
+						end: 1000,
+						onStart: function(options) {
+							contentFrameMgr.show(index);
+						}
+					});
+				}(i);
+			}
+
+			// play the video right away
+			pop.play();
+
+
 			var contentFrameMgr = new ContentFrameMgr(data.contentFrames, "contentFrameReveal", "contentFrameThumbnailReveal");
 			contentFrameMgr.show(0);
 		});
