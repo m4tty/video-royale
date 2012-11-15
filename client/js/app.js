faf79b3e7a616cf5beeef60d4d346fe6defcb479
SCHWTA2 <timothys@ecollege.com>
2012-11-15 13:32:41 -0700

    Wiring in the royale service

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,10 @@
 var App = function() {
 
-	this.initialize = function() {
-		var contentFrames = [
-			{"contentHtml": "Slide 1a"},
-			{"contentHtml": "Slide 2"},
-			{"contentHtml": "Slide 3"},
-			{"contentHtml": "Slide 4"},
-			{"contentHtml": "Slide 5"},
-			{"contentHtml": "Slide 6"},
-			{"contentHtml": "Slide 7"}
-		];
-
-		var contentFrameMgr = new ContentFrameMgr(contentFrames, "contentFrameReveal", "contentFrameThumbnailReveal");
-		contentFrameMgr.show(0);
+	this.initialize = function(videoId) {
+		$.get("/videos/" + videoId + "/royale", function(data){
+			var contentFrameMgr = new ContentFrameMgr(data.contentFrames, "contentFrameReveal", "contentFrameThumbnailReveal");
+			contentFrameMgr.show(0);
+		});
 	};
 
 };
