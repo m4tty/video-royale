c1d8342c744270bde04175be45b513a5cb3dc32d
Mac Angell <maca@ecollege.com>
2012-11-16 09:44:33 -0700

    try again

--- client/js/DefaultView.xd.js
+++ client/js/DefaultView.xd.js
@@ -7078,7 +7078,7 @@ dojo.declare("videoroyale.DefaultView", [dijit.layout.ContentPane, dijit._Templa
 				courseId: this.courseId,
 				menuItemId: this.menuItemId
 			};
-			this.iframeNode.postMessage(JSON.stringify(data), _getOriginOfUrl(this.linkedContentUrl));
+			this.iframeNode.contentWindow.postMessage(JSON.stringify(data), _getOriginOfUrl(this.linkedContentUrl));
 		}
 		else {
 			if ("console" in window) console.log("Something went horribly wrong");
