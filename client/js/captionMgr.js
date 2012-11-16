112d21da4533bf17027f898e2c5f9428158546f6
SCHWTA2 <timothys@ecollege.com>
2012-11-16 08:45:35 -0700

    Tweaking the captions

--- client/js/captionMgr.js
+++ client/js/captionMgr.js
@@ -5,10 +5,19 @@ var CaptionMgr = function(captionDivId) {
 	this.show = function(caption) {
 		captionDiv.css("opacity", 1);
 		captionDiv.html(caption.text);
+
+/*		var i = 0;
+		var intervalId = setInterval(function() {
+			captionDiv.html(caption.text.substr(0, i++));
+
+			if(i > caption.text.length) {
+				clearInterval(intervalId);
+			}	
+		}, 15);
+*/
 	};
 	
 	this.hide = function() {
 		captionDiv.css("opacity", 0);
-		captionDiv.html("");
 	};
 };
